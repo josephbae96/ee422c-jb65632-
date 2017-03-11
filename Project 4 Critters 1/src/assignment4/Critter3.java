2e7af287c96a372fc72fc3fd467f5d93ebda7f61
Vibhu Appalaraju <vibhuappalaraju@gmail.com>
2017-03-11 17:44:42 -0600

    put the header for my critters

--- Project 4 Critters 1/src/assignment4/Critter3.java
+++ Project 4 Critters 1/src/assignment4/Critter3.java
@@ -1,4 +1,17 @@
 package assignment4;
+/* CRITTERS Critter.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Vibhu Appalaraju
+ * vka249
+ * 16235
+ * Joseph Bae
+ * jb65632
+ * 16235
+ * Git URL: https://github.com/josephbae96/ee422c-jb65632-.git
+ * Slip days used: <0>
+ * Spring 2017
+ */
 
 
 

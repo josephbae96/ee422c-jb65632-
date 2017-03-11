093ef8daba60508c5cef7f2033270407d94ea5e8
josephbae96 <josephbae@utexas.edu>
2017-03-11 16:04:30 -0600

    critter.java comments

--- Project 4 Critters 1/src/assignment4/Critter.java
+++ Project 4 Critters 1/src/assignment4/Critter.java
@@ -53,6 +53,14 @@ public abstract class Critter {
 	private int x_coord;
 	private int y_coord;
 	
+	/**
+	 * Walk moves the critter once in a specified direction by changing it's specified x and y coordinates once by subtraction or addition.
+	 * Some directions do not need to have their x or y direction changed
+	 * 
+	 * @param direction
+	 * Doesn't return anything because it's void, changes position of the cirtter though
+	 * Does not throw any exceptions
+	 */
 	protected final void walk(int direction) {
 		this.energy -= Params.walk_energy_cost;
 		
@@ -119,9 +127,16 @@ public abstract class Critter {
 		}
 	}
 	
+	/**
+	 * Run moves the critter in a specified direction by adding or subtracting 2 to the appropriate x and y coordinates
+	 * Some directions do not need their x or y direction to be changed
+	 * 
+	 * @param direction
+	 * Doesn't return anything because it's void, changes the position of the critter though
+	 * Does not throw any exceptions
+	 */
 	protected final void run(int direction) {
 		this.energy -= Params.run_energy_cost;
-		
 		if(!hasMoved){
 			switch(direction){
 				case 0:	this.x_coord += 2;
@@ -185,6 +200,14 @@ public abstract class Critter {
 		}
 	}
 	
+	/**
+	* reproduce first checks if the critter has the minimum amount of energy to reproduce the critter
+	* If there is enough energy, the baby is created and then moved into the spot the direction specified
+	*
+	* @param offspring, direction
+	* Does not return anything since the method is void
+	* 
+	*/
 	protected final void reproduce(Critter offspring, int direction) {
 		if(this.energy<Params.min_reproduce_energy){
 			return;
@@ -239,55 +262,28 @@ public abstract class Critter {
 //		population.add(critters);
 		
 		Critter critters;
-
 		try{
-
-		Class<?> mycritter = Class.forName(Critter.myPackage +"."+ critter_class_name);
-
-		critters = (Critter)mycritter.newInstance();
-
-
+			Class<?> mycritter = Class.forName(Critter.myPackage +"."+ critter_class_name);
+			critters = (Critter)mycritter.newInstance();
 		}
-
 		catch(Exception e){
-
-
-		throw new InvalidCritterException(critter_class_name);
-
+			throw new InvalidCritterException(critter_class_name);
 		}
-
-		critters.x_coord=Critter.getRandomInt(Params.world_width);
-
-				
+		critters.x_coord=Critter.getRandomInt(Params.world_width);	
 		if(critters.x_coord==0)  {
-
-		critters.x_coord=critters.x_coord+Params.world_width;
-
+			critters.x_coord=critters.x_coord+Params.world_width;
 		}
-
 		if(critters.x_coord==Params.world_width+1)  {
-
-		critters.x_coord=critters.x_coord-Params.world_width;
-
+			critters.x_coord=critters.x_coord-Params.world_width;
 		}
-
 		critters.y_coord=Critter.getRandomInt(Params.world_height);
-				
-
 		if(critters.y_coord==0)  {
-
-		critters.y_coord=critters.y_coord+Params.world_height;
-
+			critters.y_coord=critters.y_coord+Params.world_height;
 		}
-
 		if(critters.y_coord==Params.world_height+1)  {
-
-		critters.y_coord=critters.y_coord-Params.world_height;
-
+			critters.y_coord=critters.y_coord-Params.world_height;
 		}
-
 		critters.energy=Params.start_energy;
-
 		population.add(critters);
 	}
 	
@@ -372,6 +368,11 @@ public abstract class Critter {
 		 * ArrayList that has been provided in the starter code.  In any case, it has to be
 		 * implemented for grading tests to work.
 		 */
+		
+		/**
+		 * getPopulation returns the critter population list
+		 * @return
+		 */
 		protected static List<Critter> getPopulation() {
 			return population;
 		}
@@ -381,6 +382,11 @@ public abstract class Critter {
 		 * ArrayList that has been provided in the starter code.  In any case, it has to be
 		 * implemented for grading tests to work.  Babies should be added to the general population 
 		 * at either the beginning OR the end of every timestep.
+		 */
+		
+		/**
+		 * getBabies returns the list of critter babies list
+		 * @return
 		 */
 		protected static List<Critter> getBabies() {
 			return babies;
@@ -392,10 +398,19 @@ public abstract class Critter {
 	 */
 	public static void clearWorld() {
 		// Complete this method.
-		population.clear();
-		
+		population.clear();	
 	}
 	
+	/**
+	 * worldTimeStep first does the doTimeStep for all the critters, then removes critters with energy <=0,then checks if two critters are in the same spot
+	 * if two critters are in the same spot, it checks for three options, both critters fighting in which case it rolls a number and the high number wins and the lower number critter dies
+	 * one critter not wanting to fight and the other critter wanting to fight, making the critter that doesn't want to fight die
+	 * and the final case being neither critter wants to fight in which the first critter is just removed
+	 * critters are then checked again for energy <= 0 in which case they're removed
+	 * algae is then created and babies are then created
+	 * the HasMoved boolean is then set to false for every critter in the population
+	 * 
+	 */
 	public static void worldTimeStep() {
 		for(Critter mybbycritter:population){
 			mybbycritter.doTimeStep();
@@ -415,7 +430,6 @@ public abstract class Critter {
 				continue;
 			}
 			for(int j=0;j<population.size();j++){
-			
 				Critter Ch=population.get(j);
 				if (Ch.energy <= 0) {
 					population.remove(j);
@@ -427,35 +441,35 @@ public abstract class Critter {
 					boolean firstfight = C.fight(Ch.toString());
 					boolean secondfight = Ch.fight(C.toString());
 					if((C.x_coord==Ch.x_coord) &&(C.y_coord==Ch.y_coord)){
-					if(firstfight && secondfight){	
-						int roll1 = Critter.getRandomInt(C.energy);
-						int roll2 = Critter.getRandomInt(Ch.energy);
-						if(roll1>=roll2){
+						if(firstfight && secondfight){	
+							int roll1 = Critter.getRandomInt(C.energy);
+							int roll2 = Critter.getRandomInt(Ch.energy);
+							if(roll1>=roll2){
+								C.energy=C.energy + (Ch.energy)/2;
+								population.remove(j);
+								i=0;
+								break;
+							}
+							else{
+								Ch.energy=Ch.energy + (C.energy)/2;
+								population.remove(i);
+								i=0;
+								break;
+							}
+						}
+						else if(firstfight && !secondfight){
 							C.energy=C.energy + (Ch.energy)/2;
 							population.remove(j);
 							i=0;
 							break;
 						}
-						else{
+						else if(!firstfight && secondfight){
 							Ch.energy=Ch.energy + (C.energy)/2;
 							population.remove(i);
 							i=0;
 							break;
-						}
+						}		
 					}
-					else if(firstfight && !secondfight){
-						C.energy=C.energy + (Ch.energy)/2;
-						population.remove(j);
-						i=0;
-						break;
-					}
-					else if(!firstfight && secondfight){
-						Ch.energy=Ch.energy + (C.energy)/2;
-						population.remove(i);
-						i=0;
-						break;
-					}		
-				}
 				}
 			}	
 		}
@@ -477,12 +491,9 @@ public abstract class Critter {
 				e.toString();
 			}
 		}
-		
 		for(Critter baby:babies){
 			population.add(baby);
 		}
-		
-		
 		babies.clear();
 		for(Critter c : population){			
 			c.hasMoved = false;
@@ -492,6 +503,9 @@ public abstract class Critter {
 		// Complete this method.
 	} // end of world time step
 	
+	/**
+	 * first the borders are created then all the critters are displayed
+	 */
 	public static void displayWorld() {
 		
 		String[][] displayofmyworld = new String[Params.world_width + 2][Params.world_height + 2];
@@ -507,34 +521,25 @@ public abstract class Critter {
 				displayofmyworld[0][x] = verticalbar;
 				displayofmyworld[Params.world_width + 1][x] = verticalbar;
 			}
-
 		}
-
 		for (int x = 0; x < Params.world_width + 1; x++) {
-
 			 if (x != 0 && x != Params.world_width + 1) {
-
 				displayofmyworld[x][0] = dash;
 				displayofmyworld[x][Params.world_height + 1] = dash;
 			}
 		}
-
 		for (int y = 1; y < Params.world_height + 1; y++) {
 			for (int x = 1; x < Params.world_width + 1; x++) {
 				displayofmyworld[x][y] = " ";
 			}
 		}
-		
 		for(int i=0;i<population.size();i++){
 			displayofmyworld[population.get(i).x_coord][population.get(i).y_coord]=population.get(i).toString();	
 		}
-
 		for (int y =0; y<Params.world_height + 2; y++) {
 			for (int x = 0; x < Params.world_width + 2; x++) {
-
 				if (x == Params.world_width + 1) {
 					System.out.println(displayofmyworld[x][y]);
-
 				} else {
 					System.out.print(displayofmyworld[x][y]);
 				}
